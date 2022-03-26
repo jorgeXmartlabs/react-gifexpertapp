ab0fa801149d691b6211881df775aab015065d3d
Jorge Luis <jorgeluis.jimenez@xmartlabs.com>
2022-03-25 22:40:58 -0300

    remove button

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -13,10 +13,8 @@ export const GifExpertApp = () => {
     return (
         <>
             <h2>GifExpertApp</h2>
-            <AddCategory setCategories={ setCategories }/>
+                <AddCategory setCategories={ setCategories }/>
             <hr />
-
-            <button onClick={ handleAdd }>Add</button>
 
             <ol>
                 {
